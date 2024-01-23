1c2de1f750f1be6710594ee1ad02c948d411312f
hafed <hafed.zouyed@univ-constantine2.dz>
2024-01-23 18:11:30 +0100

    I added the home page

---
+++ script.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+// when someone clicks the hamburger button
+navToggle.addEventListener('click', () => {
+
+  const visibility = nav.getAttribute('data-visible');
+  // if the nav is closed, open it
+  if (visibility === 'false') {
+    nav.setAttribute('data-visible', true);
+    navToggle.setAttribute("aria-expanded", true);
+  } 
+  // if the nav is open, close it
+  else {
+    nav.setAttribute('data-visible', false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+
+})
